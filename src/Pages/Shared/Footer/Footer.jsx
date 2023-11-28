1ba7f2accf6cb51c3b62ec7336463b50598c24eb
Akas_Datta <012akasdatta@gmail.com>
2023-11-29 05:42:42 +0600

    footer some code added

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -2,9 +2,115 @@
 
 const Footer = () => {
     return (
-        <div>
-            
-            <h2 className="mx-10 text-red-200 bg-green-600">footer</h2>
+        <div className="bg-[#1E1E1E] text-white">
+            <footer >
+                <div className="mx-auto max-w-screen-xl px-4 pt-8 pb-16 sm:px-6 lg:px-8">
+                    <div className="lg:flex lg:items-start lg:gap-8">
+
+                    <div
+                        className="grid grid-cols-2 gap-8 lg:mt-0 lg:grid-cols-5 lg:gap-y-16"
+                    >
+                        <div className="col-span-1 sm:col-span-1">
+                            <p className="font-bold text-gray-900">Explore</p>
+
+                            <ul className="mt-6 space-y-4 text-sm">
+                                <li>
+                                <a href="#" className="text-blue-800 transition hover:opacity-75">
+                                    Home
+                                </a>
+                                </li>
+
+                                <li>
+                                <a href="#" className="text-blue-800 transition hover:opacity-75">
+                                    Colleges
+                                </a>
+                                </li>
+
+                                <li>
+                                <a href="#" className="text-blue-800 transition hover:opacity-75">
+                                    Admission
+                                </a>
+                                </li>
+
+                                <li>
+                                <a href="#" className="text-blue-800 transition hover:opacity-75">
+                                    My College
+                                </a>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <div className="col-span-1 sm:col-span-1">
+                            <p className="font-bold text-gray-900">Services</p>
+
+                            <ul className="mt-6 space-y-4 text-sm">
+                                <li>
+                                <a href="#" className="text-blue-800 transition hover:opacity-75">
+                                    Become a Teacher
+                                </a>
+                                </li>
+
+                                <li>
+                                <a href="#" className="text-blue-800 transition hover:opacity-75">
+                                    Privacy Policy
+                                </a>
+                                </li>
+
+                                <li>
+                                <a href="#" className="text-blue-800 transition hover:opacity-75">
+                                    Blog
+                                </a>
+                                </li>
+                                <li>
+                                <a href="#" className="text-blue-800 transition hover:opacity-70">
+                                    Our Teachers
+                                </a>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <div className="col-span-1 sm:col-span-1">
+                            <p className="font-bold text-gray-900">About CampusGo University Website</p>
+
+                            <ul className="mt-6 space-y-4 text-sm">
+                                <p className="text-gray-600">
+                                    CampusGo is a comprehensive university website providing easy access to academic resources and campus information for students and faculty.</p>
+                            </ul>
+                        </div>
+                    </div>
+                    </div>
+
+                    <div className="mt-8 border-t border-blue-800 pt-8">
+                    <div className="sm:flex sm:justify-between">
+                        <p className="text-xs text-black">
+                        &copy; 2022. CampusGo. All rights reserved.
+                        </p>
+
+                        <ul
+                        className="mt-8 flex flex-wrap justify-start gap-4 text-xs sm:mt-0 lg:justify-end"
+                        >
+                        <li>
+                            <a href="#" className="text-blue-800 transition hover:opacity-75">
+                            Terms & Conditions
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="#" className="text-blue-800 transition hover:opacity-75">
+                            Privacy Policy
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="#" className="text-blue-800 transition hover:opacity-75">
+                            Cookies
+                            </a>
+                        </li>
+                        </ul>
+                    </div>
+                    </div>
+                </div>
+            </footer>
         </div>
     );
 };
