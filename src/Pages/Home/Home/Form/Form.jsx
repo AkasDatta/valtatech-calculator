9a405984db94e3dbad1ab36912f5f4d4bb68ebe9
Akas_Datta <012akasdatta@gmail.com>
2023-11-29 04:34:53 +0600

    from done

--- src/Pages/Home/Home/Form/Form.jsx
+++ src/Pages/Home/Home/Form/Form.jsx
@@ -6,11 +6,11 @@ const Form = () => {
                 <h2 className="text-4xl font-semibold">Wow $0! That's some hefty savings. Drop your email below for a free consultation with our automation experts.</h2>
 
 
-                <div className="border-b border-gray-900/10 pb-12">
+                <div className="pb-12">
 
                     <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                             <div className="sm:col-span-2 sm:col-start-1">
-                                <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">
+                                <label htmlFor="name" className="block text-xl font-medium leading-6 text-[#444444]">
                                     Name
                                 </label>
                                 <div className="mt-2">
@@ -19,37 +19,40 @@ const Form = () => {
                                         name="city"
                                         id="city"
                                         autoComplete="address-level2"
-                                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 bg-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                        placeholder="Name"
+                                        className="block w-full rounded-md border-0 py-1.5 text-[#444444] bg-white shadow-sm ring-0 ring-inset ring-white placeholder:text-gray-400 focus:ring-none focus:ring-white sm:text-lg sm:leading-10 p-3"
                                     />
                                 </div>
                             </div>
 
                             <div className="sm:col-span-2">
-                                <label htmlFor="company" className="block text-sm font-medium leading-6 text-gray-900">
+                                <label htmlFor="company" className="block text-xl font-medium leading-6 text-[#444444]">
                                     Company
                                 </label>
                                 <div className="mt-2">
                                     <input
                                     type="text"
                                     name="company"
+                                    placeholder="Company"
                                     id="company"
                                     autoComplete="address-level1"
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 bg-white shadow-sm ring-0 ring-inset ring-white placeholder:text-gray-400 focus:ring-none focus:ring-white sm:text-lg sm:leading-10 p-3"
                                     />
                                 </div>
                             </div>
 
                             <div className="sm:col-span-2">
-                                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                                <label htmlFor="email" className="block text-xl font-medium leading-6 text-gray-900">
                                     Email
                                 </label>
                                 <div className="mt-2">
                                     <input
                                     type="email"
                                     name="email"
+                                    placeholder="Email"
                                     id="email"
                                     autoComplete="email"
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 bg-white shadow-sm ring-0 ring-inset ring-white placeholder:text-gray-400 focus:ring-none focus:ring-white sm:text-lg sm:leading-10 p-3"
                                     />
                                 </div>
                             </div>
