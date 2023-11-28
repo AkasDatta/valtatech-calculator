0c19c7bc5d9064643e45e9b606d4b02e8859b73e
Akas_Datta <012akasdatta@gmail.com>
2023-11-29 05:38:37 +0600

    faq component done

--- src/Pages/Home/Home/Faq/Faq.jsx
+++ src/Pages/Home/Home/Faq/Faq.jsx
@@ -4,14 +4,14 @@ const Faq = () => {
     return (
         <div className="bg-white text-[#444444]">
             <div className="max-w-screen-2xl mx-auto lg:px-24 md:px-5 px-5 overflow-x-hidden py-24">
-                <h2 className="text-[#00876D] text-4xl font-bold">FAQ</h2>
+                <h2 className="text-[#00876D] text-4xl font-bold mb-8">FAQ</h2>
                 <div className="divide-y divide-gray-100 rounded-xl border border-gray-100 bg-white"
                     >
-                    <details className="group p-6 [&_summary::-webkit-details-marker]:hidden" open>
-                        <summary
+                    <details className="group p-6 [&_summary::-webkit-details-marker]:hidden">
+                    <summary
                         className="flex cursor-pointer items-center text-gray-900"
                         >
-                        <span className="relative h-5 w-5 shrink-0">
+                        <span className="relative h-5 w-5 shrink-0 text-[#00876D] text-xl">
                             <div
                             className="absolute inset-0 h-5 w-5 opacity-100 group-open:opacity-0"
                             stroke="currentColor"
@@ -27,62 +27,216 @@ const Faq = () => {
                             <FaChevronDown></FaChevronDown>
                             </div>
                         </span>
-                        <h2 className="text-lg font-medium mb-1 mx-2">
+                        <h2 className="text-xl font-medium mb-1 mx-4">
                         What does this Invoice Calculator do?
                         </h2>
                         </summary>
 
-                        <p className="mt-4 leading-relaxed text-gray-700">
+                        <p className="mt-4 leading-relaxed text-gray-700 text-xl">
                         In short, it will show you the cost savings of Accounts Payable Automation. Every business in New Zealand is different with how they manually process invoices so we’ve made the calculator editable in regards to how much your invoices currently cost to process.
                         </p>
                     </details>
-
                     <details className="group p-6 [&_summary::-webkit-details-marker]:hidden">
-                        <summary
-                        className="flex cursor-pointer items-center justify-between gap-1.5 text-gray-900"
+                    <summary
+                        className="flex cursor-pointer items-center text-gray-900"
                         >
-                        <h2 className="text-lg font-medium">
-                            Lorem ipsum dolor sit amet consectetur adipisicing?
-                        </h2>
-
-                        <span className="relative h-5 w-5 shrink-0">
-                            <svg
-                            xmlns="http://www.w3.org/2000/svg"
+                        <span className="relative h-5 w-5 shrink-0 text-[#00876D] text-xl">
+                            <div
                             className="absolute inset-0 h-5 w-5 opacity-100 group-open:opacity-0"
-                            fill="none"
-                            viewBox="0 0 24 24"
                             stroke="currentColor"
                             strokeWidth="2"
                             >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                            />
-                            </svg>
-
-                            <svg
-                            xmlns="http://www.w3.org/2000/svg"
+                            <FaAngleRight></FaAngleRight>                   
+                            </div>
+                            <div
                             className="absolute inset-0 h-5 w-5 opacity-0 group-open:opacity-100"
-                            fill="none"
-                            viewBox="0 0 24 24"
                             stroke="currentColor"
                             strokeWidth="2"
                             >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                            />
-                            </svg>
+                            <FaChevronDown></FaChevronDown>
+                            </div>
                         </span>
+                        <h2 className="text-xl font-medium mb-1 mx-4">
+                        How accurate is this AP Automation savings calculator?
+                        </h2>
                         </summary>
 
-                        <p className="mt-4 leading-relaxed text-gray-700">
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis
-                        molestias culpa in, recusandae laboriosam neque aliquid libero nesciunt
-                        voluptate dicta quo officiis explicabo consequuntur distinctio corporis
-                        earum similique!
+                        <p className="mt-4 leading-relaxed text-gray-700 text-xl">
+                        It’s gone through rigorous testing and checks. That alongside our years of experience automating invoice procedures. However, it is to be taken with a pinch of salt. There are so many things that can affect your cost savings, it is a great guide to start with though. When you’re ready, get in touch for a more detailed ROI.
+                        </p>
+                    </details>
+                    <details className="group p-6 [&_summary::-webkit-details-marker]:hidden">
+                    <summary
+                        className="flex cursor-pointer items-center text-gray-900"
+                        >
+                        <span className="relative h-5 w-5 shrink-0 text-[#00876D] text-xl">
+                            <div
+                            className="absolute inset-0 h-5 w-5 opacity-100 group-open:opacity-0"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            >
+                            <FaAngleRight></FaAngleRight>                   
+                            </div>
+                            <div
+                            className="absolute inset-0 h-5 w-5 opacity-0 group-open:opacity-100"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            >
+                            <FaChevronDown></FaChevronDown>
+                            </div>
+                        </span>
+                        <h2 className="text-xl font-medium mb-1 mx-4">
+                        Where did the $6 average manual invoice cost come from?
+                        </h2>
+                        </summary>
+
+                        <p className="mt-4 leading-relaxed text-gray-700 text-xl">
+                        In short, it will show you the cost savings of Accounts Payable Automation. Every business in New Zealand is different with how they manually process invoices so we’ve made the calculator editable in regards to how much your invoices currently cost to process.
+                        </p>
+                    </details>
+                    <details className="group p-6 [&_summary::-webkit-details-marker]:hidden">
+                    <summary
+                        className="flex cursor-pointer items-center text-gray-900"
+                        >
+                        <span className="relative h-5 w-5 shrink-0 text-[#00876D] text-xl">
+                            <div
+                            className="absolute inset-0 h-5 w-5 opacity-100 group-open:opacity-0"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            >
+                            <FaAngleRight></FaAngleRight>                   
+                            </div>
+                            <div
+                            className="absolute inset-0 h-5 w-5 opacity-0 group-open:opacity-100"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            >
+                            <FaChevronDown></FaChevronDown>
+                            </div>
+                        </span>
+                        <h2 className="text-xl font-medium mb-1 mx-4">
+                        How can I work out my manual invoice cost?
+                        </h2>
+                        </summary>
+
+                        <p className="mt-4 leading-relaxed text-gray-700 text-xl">
+                        You’ll need detailed data at the ready. Firstly your staff costs, and not just AP specialists, Accounting managers along with Financial Controllers will often spend a certain amount of time on general AP. Next you’ll need to figure out the cost of time wasted on chasing approvals and tracking down invoices (the good news is this cost is completely removed with Esker!) You’ll also need current software costs. The best way would be to work this out for a 30 day period, then divide that total cost by the amount of invoices processed in the same time period.
+                        </p>
+                    </details>
+                    <details className="group p-6 [&_summary::-webkit-details-marker]:hidden">
+                    <summary
+                        className="flex cursor-pointer items-center text-gray-900"
+                        >
+                        <span className="relative h-5 w-5 shrink-0 text-[#00876D] text-xl">
+                            <div
+                            className="absolute inset-0 h-5 w-5 opacity-100 group-open:opacity-0"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            >
+                            <FaAngleRight></FaAngleRight>                   
+                            </div>
+                            <div
+                            className="absolute inset-0 h-5 w-5 opacity-0 group-open:opacity-100"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            >
+                            <FaChevronDown></FaChevronDown>
+                            </div>
+                        </span>
+                        <h2 className="text-xl font-medium mb-1 mx-4">
+                        What is the average manual invoice cost?
+                        </h2>
+                        </summary>
+
+                        <p className="mt-4 leading-relaxed text-gray-700 text-xl">
+                        The average manual invoice cost we have seen from our customers has averaged between $6 and $12. The common number we came across online is $15 per invoice. This was based across a variety of industries and included some paper based invoice costs too.
+                        </p>
+                    </details>
+                    <details className="group p-6 [&_summary::-webkit-details-marker]:hidden">
+                    <summary
+                        className="flex cursor-pointer items-center text-gray-900"
+                        >
+                        <span className="relative h-5 w-5 shrink-0 text-[#00876D] text-xl">
+                            <div
+                            className="absolute inset-0 h-5 w-5 opacity-100 group-open:opacity-0"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            >
+                            <FaAngleRight></FaAngleRight>                   
+                            </div>
+                            <div
+                            className="absolute inset-0 h-5 w-5 opacity-0 group-open:opacity-100"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            >
+                            <FaChevronDown></FaChevronDown>
+                            </div>
+                        </span>
+                        <h2 className="text-xl font-medium mb-1 mx-4">
+                        How can I reduce invoice costs?
+                        </h2>
+                        </summary>
+
+                        <p className="mt-4 leading-relaxed text-gray-700 text-xl">
+                        Automation! Our Accounts Payable Automation solution using Esker is our method of choice, it’s beautifully complex with a simple UI. Check out our <span className="cursor-pointer text-[#00876D]">case studies</span> on how some of New Zealand’s biggest brands have been transformed by FUJIFILM Process Automation.
+                        </p>
+                    </details>
+                    <details className="group p-6 [&_summary::-webkit-details-marker]:hidden">
+                    <summary
+                        className="flex cursor-pointer items-center text-gray-900"
+                        >
+                        <span className="relative h-5 w-5 shrink-0 text-[#00876D] text-xl">
+                            <div
+                            className="absolute inset-0 h-5 w-5 opacity-100 group-open:opacity-0"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            >
+                            <FaAngleRight></FaAngleRight>                   
+                            </div>
+                            <div
+                            className="absolute inset-0 h-5 w-5 opacity-0 group-open:opacity-100"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            >
+                            <FaChevronDown></FaChevronDown>
+                            </div>
+                        </span>
+                        <h2 className="text-xl font-medium mb-1 mx-4">
+                        What is accounts payable automation?
+                        </h2>
+                        </summary>
+
+                        <p className="mt-4 leading-relaxed text-gray-700 text-xl">
+                        It’s a life saver. It minimises and in some cases eliminates human interaction with invoices, all while processing them error-free and super quick. We use Esker to do this, providing OCR scanning and AI technology that adapts to new and changing invoices, giving you handy alerts for the important bits like bank accounts changing and PO’s not matched.
+                        </p>
+                    </details>
+                    <details className="group p-6 [&_summary::-webkit-details-marker]:hidden">
+                    <summary
+                        className="flex cursor-pointer items-center text-gray-900"
+                        >
+                        <span className="relative h-5 w-5 shrink-0 text-[#00876D] text-xl">
+                            <div
+                            className="absolute inset-0 h-5 w-5 opacity-100 group-open:opacity-0"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            >
+                            <FaAngleRight></FaAngleRight>                   
+                            </div>
+                            <div
+                            className="absolute inset-0 h-5 w-5 opacity-0 group-open:opacity-100"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            >
+                            <FaChevronDown></FaChevronDown>
+                            </div>
+                        </span>
+                        <h2 className="text-xl font-medium mb-1 mx-3">
+                        How do I get started with Automating my AP?
+                        </h2>
+                        </summary>
+
+                        <p className="mt-4 leading-relaxed text-gray-700 text-xl">
+                        Now that you’ve seen how much you could save with our invoice automation savings calculator, we’re sure you’ll want to get going! Chat with our expert, local team today. They’ll go through the finer details of your situation and tailor the solution to you and your business. 
                         </p>
                     </details>
                 </div>
