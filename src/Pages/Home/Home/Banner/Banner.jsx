3ff9ce5b17e163d5ecd6d580078a3657dffe1023
Akas_Datta <012akasdatta@gmail.com>
2023-11-29 03:02:03 +0600

    banner card responsiveness modified

--- src/Pages/Home/Home/Banner/Banner.jsx
+++ src/Pages/Home/Home/Banner/Banner.jsx
@@ -1,4 +1,6 @@
-// Banner.js
+import drug from "../../../../assets/efficiency.png";
+import calculator from "../../../../assets/calculator.png";
+import network from "../../../../assets/network.png";
 import './Banner.css'; // Import your CSS file
 
 const Banner = () => {
@@ -20,6 +22,36 @@ const Banner = () => {
                             Our handy interactive invoice calculator shows you just how much money you
                             could be saving by automating your Accounts Payable. Spoiler … it’s a lot.
                         </p>
+                    </div>
+                </div>
+
+                <div className='grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3 gap-20 my-16'>
+                    <div className="xl:flex rounded-xl bg-white text-black shadow-2xl">
+                        <div>
+                        <figure><img src={drug} className="w-14 mx-8 mt-8 xl:mx-12" alt="drug"/></figure>
+                        </div>
+                        <div className="card-body">
+                            <h2 className="font-semibold text-3xl text-[#007957]">Step 1</h2>
+                            <p className="text-xl">Drag the pointer to match your monthly invoice count.</p>
+                        </div>
+                    </div>
+                    <div className="lg:flex rounded-xl bg-white text-black shadow-2xl">
+                        <div>
+                        <figure><img src={drug} className="w-14 mx-8 mt-8 xl:mx-12" alt="drug"/></figure>
+                        </div>
+                        <div className="card-body">
+                            <h2 className="font-semibold text-3xl text-[#007957]">Step 1</h2>
+                            <p className="text-xl">Drag the pointer to match your monthly invoice count.</p>
+                        </div>
+                    </div>
+                    <div className="lg:flex rounded-xl bg-white text-black shadow-2xl">
+                        <div>
+                        <figure><img src={drug} className="w-14 mx-8 mt-8 xl:mx-12" alt="drug"/></figure>
+                        </div>
+                        <div className="card-body">
+                            <h2 className="font-semibold text-3xl text-[#007957]">Step 1</h2>
+                            <p className="text-xl">Drag the pointer to match your monthly invoice count.</p>
+                        </div>
                     </div>
                 </div>
             </div>
