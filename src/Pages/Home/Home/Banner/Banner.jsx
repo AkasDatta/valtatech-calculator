b299c4e505ea09450981630482f115e16f07f07b
Akas_Datta <012akasdatta@gmail.com>
2023-11-29 20:11:56 +0600

    banner and calculator some file move each other

--- src/Pages/Home/Home/Banner/Banner.jsx
+++ src/Pages/Home/Home/Banner/Banner.jsx
@@ -2,8 +2,16 @@ import drug from "../../../../assets/efficiency.png";
 import calculator from "../../../../assets/calculator.png";
 import network from "../../../../assets/network.png";
 import './Banner.css'; // Import your CSS file
+import { useState } from "react";
+import { FaAnglesDown } from "react-icons/fa6";
 
 const Banner = () => {
+    const [isHovered, setHovered] = useState(false);
+
+    const handleHover = () => {
+        setHovered(!isHovered);
+    };
+
     return (
         <div className="bg-[#009f72] text-white">
             <div className="max-w-screen-2xl mx-auto lg:px-24 md:px-5 px-5 overflow-x-hidden pt-12">
@@ -52,6 +60,16 @@ const Banner = () => {
                             <h2 className="font-semibold text-3xl text-[#007957]">Step 3</h2>
                             <p className="text-xl">Marvel at the cost savings and get in touch to get started.</p>
                         </div>
+                    </div>
+                </div>
+
+                <div className="text-center">
+                    <div
+                        className={`icon-container ${isHovered ? 'hovered' : ''}`}
+                        onMouseEnter={handleHover}
+                        onMouseLeave={handleHover}
+                    >
+                        <FaAnglesDown className="text-6xl text-[#0B6550] icon"></FaAnglesDown>
                     </div>
                 </div>
             </div>
