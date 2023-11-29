ece5845762fc829c287f447d82a0b9af0073eaa9
Akas_Datta <012akasdatta@gmail.com>
2023-11-29 21:30:34 +0600

    react-circular-slider added and something changed

--- src/Pages/Home/Home/Calculator/Calculator.jsx
+++ src/Pages/Home/Home/Calculator/Calculator.jsx
@@ -1,13 +1,32 @@
 
+import CircularSlider from "@fseehawer/react-circular-slider";
 import "./Calculator.css"; 
+import { FaBars } from "react-icons/fa";
 
 const Calculator = () => {
- 
-
     return (
         <div className="bg-[#009f72] text-white">
-            <div className="max-w-screen-2xl mx-auto lg:px-24 md:px-5 px-5 overflow-x-hidden py-12">
-                dd
+            <div className="max-w-screen-2xl mx-auto lg:px-24 md:px-5 px-5 overflow-x-hidden py-12 text-center">
+                <h2>Average Invoice Processing Cost ($NZD)</h2>
+                <CircularSlider
+                    label="savings"
+                    min={0}
+                    max={100}
+                    dataIndex={20}
+                    prependToValue="$"
+                    appendToValue="K"
+                    labelColor="#005a58"
+                    labelBottom={true}
+                    knobColor="#005a58"
+                    knobSize={72}
+                    progressColorFrom="#00bfbd"
+                    progressColorTo="#005a58"
+                    progressSize={24}
+                    trackColor="#eeeeee"
+                    trackSize={24}
+                >
+                    <FaBars x='22' y='22' width='28px' height='28px' />
+                </CircularSlider>
             </div>
         </div>
     );
