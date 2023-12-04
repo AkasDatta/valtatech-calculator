e3e8c1f239590cdecfd7d1cae5975a6206e341fd
Akas_Datta <012akasdatta@gmail.com>
2023-12-05 01:20:37 +0600

    alignment changed

--- src/Pages/Home/Home/Calculator/Calculator.jsx
+++ src/Pages/Home/Home/Calculator/Calculator.jsx
@@ -1,16 +1,10 @@
-import { CircleSlider } from "react-circle-slider";
-import { FaBars } from "react-icons/fa";
 import { FaArrowsLeftRight } from "react-icons/fa6";
 import { useState } from "react";
 import CircularSlider from "@fseehawer/react-circular-slider";
 import { useEffect } from "react";
-// import CircularSlider from "@fseehawer/react-circular-slider";
-// import "./Calculator.css";
-// import { useState } from "react";
 
 const Calculator = () => {
-
-const [initialCost, setInitialCost] = useState(4);
+const [initialCost, setInitialCost] = useState(6);
 const [numberOfInvoices, setNumberOfInvoices] = useState(0);
 const [result, setResult] = useState(0);
 
@@ -26,17 +20,12 @@ const calculateSavings = () => {
   return (
     <div className="bg-[#009f72] text-white">
       <div className="max-w-screen-2xl mx-auto lg:px-24 md:px-5 px-5 overflow-x-hidden py-12 text-center">
-        <h2 className="text-2xl mb-12">Average Invoice Processing<br /> Cost ($SGD)</h2>
-        <input onChange={e => setInitialCost(e.target.value)} type="number" value={initialCost} placeholder="6" className="rounded-md border-b-4 py-1.5 text-[#444444] bg-[#009f72] shadow-sm ring-0 ring-inset ring-[#009f72] placeholder:text-[#fff] focus:ring-none focus:ring-[#fff] sm:leading-10 p-3" />
-        {/* <div className="textContainer">
-          {value}
-          <div className="minute">MINUTES</div>
-        </div> */}
-        
+        <h2 className="text-2xl">Average Invoice Processing<br /> Cost ($SGD)</h2>
 
-        <div className="border-2 mx-auto w-96 p-3 rounded-full circle-slider-container">
+        <input onChange={e => setInitialCost(e.target.value)} type="number" value={initialCost} placeholder="6" className="rounded-md border-b-4 py-1.5 text-[#fff] text-center text-2xl font-bold bg-[#009f72] shadow-sm ring-0 ring-inset ring-[#009f72] placeholder:text-[#fff] focus:ring-none focus:ring-[#fff] sm:leading-10 mb-6" />
+        <div className="border-2 mx-auto w-80 h-80 p-3 rounded-full circle-slider-container">
           <div className="circle-slider-outer">
-             <CircularSlider
+                <CircularSlider
                     label="savings"
                     min={0}
                     max={10000}
@@ -45,17 +34,16 @@ const calculateSavings = () => {
                     appendToValue="K"
                     labelColor="#fff"
                     labelBottom={true}
-                    knobColor="#005a58"
+                    knobColor="#eeeeee"
                     knobSize={72}
-                    progressColorFrom="#00bfbd"
-                    progressColorTo="#005a58"
+                    progressColorFrom="#fff"
+                    progressColorTo="#fff"
                     progressSize={5}
                     trackColor="#eeeeee"
                     trackSize={5}
                     continuous={{interval:100}}
                     onChange={ value => { setNumberOfInvoices(value)} }
                 >
-                    <FaBars x='22' y='22' width='28px' height='28px' />
                     <FaArrowsLeftRight className="text-2xl text-black" x='24' y='24' width='32px' height='32px' />
                 </CircularSlider>
           </div>
