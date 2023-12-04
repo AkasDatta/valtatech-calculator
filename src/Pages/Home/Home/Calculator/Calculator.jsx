1208ce4c0d8df28a88c602052e9a3f387e9de7fd
Akas_Datta <012akasdatta@gmail.com>
2023-12-04 21:01:54 +0600

    something changed

--- src/Pages/Home/Home/Calculator/Calculator.jsx
+++ src/Pages/Home/Home/Calculator/Calculator.jsx
@@ -17,12 +17,13 @@ const Calculator = () => {
     <div className="bg-[#009f72] text-white">
       <div className="max-w-screen-2xl mx-auto lg:px-24 md:px-5 px-5 overflow-x-hidden py-12 text-center">
         <h2 className="text-2xl mb-12">Average Invoice Processing<br /> Cost ($SGD)</h2>
+        <input type="number" placeholder="6" className="rounded-md border-b-4 py-1.5 text-[#444444] bg-[#009f72] shadow-sm ring-0 ring-inset ring-[#009f72] placeholder:text-[#fff] focus:ring-none focus:ring-[#fff] sm:leading-10 p-3" />
         <div className="textContainer">
           {value}
           <div className="minute">MINUTES</div>
         </div>
 
-        <div className="border-2 mx-auto w-96 h- p-4 rounded-full circle-slider-container">
+        <div className="border-2 mx-auto w-96 p-3 rounded-full circle-slider-container">
           <div className="circle-slider-outer">
             <CircleSlider
               value={value}
@@ -35,7 +36,11 @@ const Calculator = () => {
               gradientColorFrom="#fff"
               gradientColorTo="#fff"
               knobRadius={20}
-              circleWidth={2}
+              circleWidth={10}
+              showPercentage={true}
+              showTooltip={true}
+              shadow={true}
+              tooltipColor="#fff"
             >
               <FaArrowsLeftRight className="text-2xl text-black" x="24" y="24" />
             </CircleSlider>
